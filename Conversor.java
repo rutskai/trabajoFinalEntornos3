d0c4b44d3e07a656fd0a8f707b0c01d54d0f91dc
Ruth <Ruth3579@gmail.com>
2025-06-05 18:48:37 +0200

    Añadimos el main

---
+++ Conversor.java
@@ -0,0 +1,20 @@
+package conversion;
+
+public class Conversor {
+
+	// Para pasar de km a millas multiplicamos por la siguiente constante:
+	private final static double KM_A_MILLAS = 0.62;
+
+	// Para pasar de milibares a atmósferas multiplicamos por la siguiente
+	// constante:
+	private final static double MB_A_ATM = 0.00098;
+
+	// Para pasar de calorías a julios multiplicamos por la siguiente constante:
+	private final static double CAL_A_JULIOS = 4.184;
+
+	// Recibe un número decimal y lo devuelve redondeado a dos decimales.
+	public static double redondear(double num) {
+		return Math.round(num * 100) / 100.0;
+	}
+
+}
